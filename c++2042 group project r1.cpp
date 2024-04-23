8c81063a96440d85cb0d822069049647495d270f
Baronguthib <167713509+Baronguthib@users.noreply.github.com>
2024-04-23 23:02:41 +0800

    Update c++2042 group project r1.cpp

--- c++2042 group project r1.cpp
+++ c++2042 group project r1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 #include <cstring>
 using namespace std;
@@ -7,9 +7,9 @@ char giftID[15][4];
 char giftDescription[15][100];
 int Price[15];
 int pointsRequired[15];
-char CustomerID[10][20];
-char CustomerRank[10][10];
-int pointsBalance[10];
+char CustomerID[20][20];
+char CustomerRank[20][20];
+int pointsBalance[20];
 
 void R1()
 {
@@ -18,9 +18,9 @@ void R1()
 	"TechCook Toaster Oven","Wellcome $50 Coupon","Mannings $50 Coupon","Carol Restaurant $100 Coupon","Shell $200 Coupon","Clever Headset","HP Optic Mouse","Stylish Bluetooth Speaker" };
 	const int P[15] = { 3900,2400,400,1500,480,1800,600,450,50,50,100,200,350,250,800 };
 	const int points[15] = { 19000,11500,2000,7300,2400,8800,2900,2250,250,250,500,960,1750,1250,3900 };
-	char Customer[10][20] = { "Tommy2015","DavidChan","Luna123","TigerMan","Max5678","Neo2000","CCTang","EchoWong","Rubychow","Ivy2023" };
-	char Rank[10][2] = { "B","B","B","B","S","S","S","G","G","G" };
-	int Balance[10] = { 8500,22800,650,14000,2580,8000,33554,8650,28000,12340 };
+	char Customer[20][20] = { "Tommy2015","DavidChan","Luna123","TigerMan","Max5678","Neo2000","CCTang","EchoWong","Rubychow","Ivy2023" };
+	char Rank[20][20] = { "B","B","B","B","S","S","S","G","G","G" };
+	int Balance[20] = { 8500,22800,650,14000,2580,8000,33554,8650,28000,12340 };
 	for (int i = 0; i < 15; i++)
 	{
 		strcpy_s(giftID[i], id[i]);
@@ -31,6 +31,7 @@ void R1()
 		strcpy_s(CustomerRank[i], Rank[i]);
 		pointsBalance[i] = Balance[i];
 	}
+	cout << "Starting Data Loaded\n\n\n";
 }
 
 int main()
